8f605edddc198599df464293cf923fdda7aa988b
gyan0890 <gyanlakshmi@gmail.com>
2022-05-22 00:23:21 +0530

    First commit of the HackMoney Contract - Currently configured to deployed on Nervos L2

---
+++ test/sample-test.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("CrypChip", function () {
+  it("Should create a new group and return user membership data", async function () {
+    const CrypChip = await ethers.getContractFactory("CrypChip");
+    const crypchip = await CrypChip.deploy();
+    await crypchip.deployed();
+
+
+    const createGroupTx = await crypchip.createGroup(["0x420674Af540BE70031cbdE8C9279fA4fF4b049CE", "0xb8761b30c951d0614459b2e227Ea944df0314665"]);
+
+    // wait until the transaction is mined
+    await createGroupTx.wait();
+
+    const groupData = await crypchip.getGroups("0x420674Af540BE70031cbdE8C9279fA4fF4b049CE");
+    expect(groupData[0].gId).to.equal(1);
+  });
+
+  it("Should create a new group, add expense and return expense data", async function () {
+    //TODO
+    const CrypChip = await ethers.getContractFactory("CrypChip");
+    const crypchip = await CrypChip.deploy();
+    await crypchip.deployed();
+
+
+    const createGroupTx = await crypchip.createGroup(["0x420674Af540BE70031cbdE8C9279fA4fF4b049CE", "0xb8761b30c951d0614459b2e227Ea944df0314665"]);
+
+    // wait until the transaction is mined
+    await createGroupTx.wait();
+
+    const groupData = await crypchip.getGroups("0x420674Af540BE70031cbdE8C9279fA4fF4b049CE");
+    expect(groupData[0].gId).to.equal(1);
+  });
+
+});
